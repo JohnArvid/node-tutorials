c87695fde749080c60f95ed39259d00d94a0bb1f
Arvid Lindström <arvid-lindstrom@live.se>
2023-11-29 21:17:08 +0100

    added button for completing, works remains

--- dothis-api/app.js
+++ dothis-api/app.js
@@ -9,8 +9,17 @@ document.addEventListener('DOMContentLoaded', () => {
 
 				tasks.forEach(task =>{
 					const listItem = document.createElement('li');
+					
 					listItem.textContent = task.task;
 					todoList.appendChild(listItem);
+
+					const completeButton = document.createElement('input');
+					completeButton.setAttribute('type', 'checkbox');
+					completeButton.classList.add('complete');
+					completeButton.setAttribute('value', task.id);
+					listItem.prepend(completeButton);
+					// add delete button 
+
 				});
 			})
 			.catch(error => console.error('Error fetching tasks: ', error));
@@ -40,6 +49,36 @@ document.addEventListener('DOMContentLoaded', () => {
 		})
 		.catch(error => console.error('Error adding task: ', error));
 
+	});
+
+	// Event listener for completing tasks
+
+	document.addEventListener('click', function (event) {
+		// event.preventDefault();
+		const target = event.target;
+		const taskId = event.target.value;
+		if (target.classList.contains('complete')) {
+			console.log(taskId);
+		}
+
+		// const taskInput = document.getElementById('newTask');
+		// const newTask = { task : taskInput.value };
+
+/*
+		fetch('http://localhost:3000/tasks/:id', {
+			method: 'PUT',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(newTask),
+		})
+		.then(response => response.json())
+		.then(() => {
+			taskInput.value = '';
+			fetchTasks(); // Refresh task list
+		})
+		.catch(error => console.error('Error adding task: ', error));
+*/
 	});
 
 });
