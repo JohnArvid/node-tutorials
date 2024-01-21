20a813cc28eb11da87b3f484a2a050e1cc60f0bc
Arvid Lindström <arvid-lindstrom@live.se>
2024-01-21 22:04:35 +0100

    all done

--- run-in-cli/script.js
+++ run-in-cli/script.js
@@ -1,5 +1,20 @@
+// node script.js
+// above in terminal to run 
+
 const fs = require('fs');
-const output = fs.readFileSync('data.txt', 'utf-8');
+const { stringify } = require('querystring');
+const output = fs.readFileSync('data.txt', 'utf-8')
+    .trim()
+    .split('\n')
+    .map(line => line.split('\t'))
+    .reduce((acc, line)=>{
+        acc[line[0]] = acc[line[0]]||[];
+        acc[line[0]].push({
+            name: line[1],
+            price: line[2],
+            quantity: line[3]
+        });
+        return acc;
+    }, {});
 
-
-console.log('hello', output);
+console.log('output: ', JSON.stringify(output, null, 2));
