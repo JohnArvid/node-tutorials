7dc524c16416c5074c752b3fd94f8cd4b7791297
Arvid Lindström <arvid-lindstrom@live.se>
2024-01-24 19:19:24 +0100

    adding to js

---
+++ cli-tool-ghinfo/cli-tool-ghinfo.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+const axios = require('axios');
+const program = require('commander');
+
+program
+    .version('1.0.0')
+    .description('My first cli-tool, fetches github user info');
+
+program
+    .requiredOption('-u, --user <username>', 'GitHub username');
+
+program.parse(process.argv);
+
+const { user } = program.opts();
+
+const apiUrl = `https://api.github.com/users/${user}`;
+
+
